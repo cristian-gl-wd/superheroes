c52160fc0f7b3d907af543a29b0be3d147b03f64
Cristian G.L. <cristian.gl.wd@gmail.com>
2024-04-03 00:46:55 +0200

    fix(app): change config to show router-outlet components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, TopBarComponent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SuperheroListComponent } from './features/superhero/superhero-list/superhero-list.component';
+import { SuperheroDetailComponent } from './features/superhero/superhero-detail/superhero-detail.component';
+import { SuperheroComponent } from './features/superhero/superhero/superhero.component';
+import { Error404Component } from './shared/components/error-404/error-404.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/superheroes',
+        pathMatch: 'full'
+    },
+    {
+        path: 'superheroes',
+        component: SuperheroComponent,
+        children: [
+            { path: '', component: SuperheroListComponent },
+            { path: 'new', component: SuperheroDetailComponent },
+            { path: ':id', component: SuperheroDetailComponent }
+        ]
+    },
+    {
+        path: 'error-404',
+        component: Error404Component
+    },
+    {
+        path: '**',
+        redirectTo: '/error-404',
+        pathMatch: 'full'
+    }
+];
