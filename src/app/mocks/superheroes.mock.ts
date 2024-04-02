8978b5679bc7e593bbdf9a0a9870a7566e4c7ea9
Cristian G.L. <cristian.gl.wd@gmail.com>
2024-04-03 00:43:32 +0200

    feat(superheroes.mock): create a file to brind data to project

---
+++ src/app/mocks/superheroes.mock.ts
@@ -0,0 +1,46 @@
+import { Superhero } from '../models/superhero.model';
+
+export const SUPERHEROES: Superhero[] = [
+    {
+        id: 1,
+        name: "Spiderman",
+        powers: ["Superfuerza", "Sentido arácnido", "Adherencia", "Lanzarredes"],
+        team: "Vengadores"
+    },
+    {
+        id: 2,
+        name: "Batman",
+        powers: ["Condición física", "Artes marciales", "Dinero"],
+        team: "Liga de la justicia"
+    },
+    {
+        id: 3,
+        name: "Superman",
+        powers: ["Superfuerza", "Vuelo", "Supervelocidad", "Invulneravilidad", "Supersentidos"],
+        team: "Liga de la justicia"
+    },
+    {
+        id: 4,
+        name: "Iron Man",
+        powers: ["Inteligencia excepcional", "Armadura tecnológicamente avanzada", "Vuelo", "Variedad de armas"],
+        team: "Vengadores"
+    },
+    {
+        id: 5,
+        name: "Thor",
+        powers: ["Superfuerza", "Durabilidad", "Control del clima", "Vuelo"],
+        team: "Vengadores"
+    },
+    {
+        id: 6,
+        name: "Hulk",
+        powers: ["Superfuerza", "Resistencia", "Regeneración", "Salto de gran alcance"],
+        team: "Vengadores"
+    },
+    {
+        id: 7,
+        name: "Wonder Woman",
+        powers: ["Superfuerza", "Vuelo", "Durabilidad", "Agilidad", "Lazo de la verdad"],
+        team: "Liga de la justicia"
+    }
+];
