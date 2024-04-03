1b0c3f647bba30d3fdc1cbccf2fc8226358221f5
Cristian G.L. <cristian.gl.wd@gmail.com>
2024-04-03 14:00:54 +0200

    test(superhero.component): make tests

--- src/app/features/superhero/superhero/superhero.component.spec.ts
+++ src/app/features/superhero/superhero/superhero.component.spec.ts
@@ -1,23 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SuperheroComponent } from './superhero.component';
 
 describe('SuperheroComponent', () => {
-  let component: SuperheroComponent;
-  let fixture: ComponentFixture<SuperheroComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SuperheroComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SuperheroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [
+        RouterTestingModule,
+        SuperheroComponent
+      ]
+    }).compileComponents();
   });
 
-  it('should create', () => {
+  it('should create the superhero component', () => {
+    const fixture = TestBed.createComponent(SuperheroComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
